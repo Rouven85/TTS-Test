656fdce6c5853c95f7c2e0da3518fc4a4f7ef646
Rouven85 <r.arheidt@gmail.com>
2024-11-04 09:22:40 +0100

    241104_DealToTable

---
+++ lua/MapTile.lua
@@ -0,0 +1,14 @@
+function deal ()
+    local mapDeck = getObjectFromGUID("8f9768")
+    local zone = getObjectFromGUID("15010e")
+    --position = mapDeck.setPosition(zone.getPosition())
+    local xPos = -15
+    print ("Hallo")
+    for i = 1, 5, 1 do
+        mapDeck.takeObject ({
+        flip = true,
+        position = {xPos, 2, -10},
+        })
+        xPos = xPos +2.5
+    end
+end
